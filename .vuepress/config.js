e9077dcf98d5f1421ca3941d73faf9adb4316c24
zhihaofans <zhihaofans@hotmail.com>
2021-01-21 11:43:50 +0800

    Add theme vuepress-theme-maker

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -1,6 +1,7 @@
 module.exports = {
   title: 'My VuePress Project',
   description: `A simple VuePress project deployed with ${PRODUCT_NAME}.`,
+  theme:'vuepress-theme-maker',
   themeConfig: {
     nav: [
       { text: 'Home', link: '/' },
