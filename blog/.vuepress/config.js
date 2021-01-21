bf18b2750ce95874ba093c618b6a2fcc0412e878
zhihaofans <zhihaofans@hotmail.com>
2021-01-21 15:16:59 +0800

    Update config.js

--- blog/.vuepress/config.js
+++ blog/.vuepress/config.js
@@ -16,63 +16,53 @@ module.exports = {
     themeConfig: {
         nav: [{ text: "🏠 Home", link: "/" }],
         searchPlaceholder: "Search",
-        searchMaxSuggestions: 10,
-        plugins: [
-            [
-                "@vuepress/search",
-                {
-                    searchMaxSuggestions: 10
-                }
-            ],
-            [
-                "@vuepress/blog",
-                {
-                    globalPagination: {
-                        prevText: "",
-                        nextText: ""
-                    },
-                    directories: [
-                        {
-                            id: "post",
-                            dirname: "_post",
-                            path: "/post/",
-                            itemPermalink: "/post/:year/:month/:day/:slug.html",
-                            pagination: {
-                                perPagePosts: 10
-                            }
-                        }
-                    ],
-                    frontmatters: [
-                        {
-                            id: "tag",
-                            keys: ["tag", "tags"],
-                            path: "/tags/",
-                            frontmatter: { title: "Tag" },
-                            pagination: {
-                                lengthPerPage: 10
-                            }
-                        },
-                        {
-                            id: "category",
-                            keys: ["category", "categories"],
-                            path: "/categories/",
-                            frontmatter: { title: "Category" },
-                            pagination: {
-                                lengthPerPage: 10
-                            }
-                        }
-                    ]
-                }
-            ]
-        ]
+        searchMaxSuggestions: 10
     },
     plugins: [
         [
-            "vuepress-plugin-container",
+            "@vuepress/search",
             {
-                type: "vue",
-                before: '<pre class="language-js"><code>',
-                after: "</code></pre>"
+                searchMaxSuggestions: 10
+            }
+        ],
+        [
+            "@vuepress/blog",
+            {
+                globalPagination: {
+                    prevText: "",
+                    nextText: ""
+                },
+                directories: [
+                    {
+                        id: "post",
+                        dirname: "_post",
+                        path: "/post/",
+                        itemPermalink: "/post/:year/:month/:day/:slug.html",
+                        pagination: {
+                            perPagePosts: 10
+                        }
+                    }
+                ],
+                frontmatters: [
+                    {
+                        id: "tag",
+                        keys: ["tag", "tags"],
+                        path: "/tags/",
+                        frontmatter: { title: "Tag" },
+                        pagination: {
+                            lengthPerPage: 10
+                        }
+                    },
+                    {
+                        id: "category",
+                        keys: ["category", "categories"],
+                        path: "/categories/",
+                        frontmatter: { title: "Category" },
+                        pagination: {
+                            lengthPerPage: 10
+                        }
+                    }
+                ]
             }
         ]
     ]
