fa2ef5b93a9750272e4106b09adcc6ee513912fd
zhihaofans <zhihaofans@hotmail.com>
2021-01-21 14:56:14 +0800

    Update config.js

--- blog/.vuepress/config.js
+++ blog/.vuepress/config.js
@@ -4,72 +4,76 @@ module.exports = {
     port: 8080,
     theme: "vuepress-theme-maker",
     markdown: {
-      lineNumbers: true,
-      extractHeaders: ["h2", "h3", "h4"],
-      plugins: {
-        "markdown-it-mark": true,
-        "markdown-it-footnote": true,
-        "markdown-it-abbr": true,
-        "markdown-it-task-lists": true
-      }
+        lineNumbers: true,
+        extractHeaders: ["h2", "h3", "h4"],
+        plugins: {
+            "markdown-it-mark": true,
+            "markdown-it-footnote": true,
+            "markdown-it-abbr": true,
+            "markdown-it-task-lists": true
+        }
     },
     themeConfig: {
-      nav: [{ text: "🏠 Home", link: "/" }],
-      searchPlaceholder: "Search",
-      searchMaxSuggestions: 10,
-      plugins: [
-        [
-          "@vuepress/search",
-          {
-            searchMaxSuggestions: 10
-          }
-        ],
-        [
-          "@vuepress/blog",
-          {
-            globalPagination: {
-              prevText: "",
-              nextText: ""
-            },
-            directories: [{
-              id: "post",
-              dirname: "_post",
-              path: "/post/",
-              itemPermalink: "/post/:year/:month/:day/:slug.html",
-              pagination: {
-                perPagePosts: 10
-              }
-            }],
-            frontmatters: [{
-                id: "tag",
-                keys: ["tag", "tags"],
-                path: "/tags/",
-                frontmatter: { title: "Tag" },
-                pagination: {
-                  lengthPerPage: 10
+        nav: [{ text: "🏠 Home", link: "/" }],
+        searchPlaceholder: "Search",
+        searchMaxSuggestions: 10,
+        plugins: [
+            [
+                "@vuepress/search",
+                {
+                    searchMaxSuggestions: 10
                 }
-              },
-              {
-                id: "category",
-                keys: ["category", "categories"],
-                path: "/categories/",
-                frontmatter: { title: "Category" },
-                pagination: {
-                  lengthPerPage: 10
+            ],
+            [
+                "@vuepress/blog",
+                {
+                    globalPagination: {
+                        prevText: "",
+                        nextText: ""
+                    },
+                    directories: [
+                        {
+                            id: "post",
+                            dirname: "_post",
+                            path: "/post/",
+                            itemPermalink: "/post/:year/:month/:day/:slug.html",
+                            pagination: {
+                                perPagePosts: 10
+                            }
+                        }
+                    ],
+                    frontmatters: [
+                        {
+                            id: "tag",
+                            keys: ["tag", "tags"],
+                            path: "/tags/",
+                            frontmatter: { title: "Tag" },
+                            pagination: {
+                                lengthPerPage: 10
+                            }
+                        },
+                        {
+                            id: "category",
+                            keys: ["category", "categories"],
+                            path: "/categories/",
+                            frontmatter: { title: "Category" },
+                            pagination: {
+                                lengthPerPage: 10
+                            }
+                        }
+                    ]
                 }
-              }
             ]
-          }
         ]
-      },
-      plugins: [
+    },
+    plugins: [
         [
-          "vuepress-plugin-container",
-          {
-            type: "vue",
-            before: '<pre class="language-js"><code>',
-            after: "</code></pre>"
-          }
+            "vuepress-plugin-container",
+            {
+                type: "vue",
+                before: '<pre class="language-js"><code>',
+                after: "</code></pre>"
+            }
         ]
-      ]
-  };
+    ]
+};
